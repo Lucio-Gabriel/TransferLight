aa8f3942880a940ba6ad530ae4e1ebce3927add0
Lucio-Dev <luciogabrieldeazevedoo@gmail.com>
2025-09-03 15:27:40 -0300

    feat(ui): create new navigation

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -23,9 +23,9 @@ new class extends Component
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    {{-- <a href="{{ route('index-user') }}" wire:navigate>
+                    <a href="{{ route('index-shopkeeper') }}" wire:navigate>
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
-                    </a> --}}
+                    </a>
                 </div>
 
                 <!-- Navigation Links -->

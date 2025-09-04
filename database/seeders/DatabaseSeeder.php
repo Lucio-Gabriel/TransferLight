c7b0fd2d99f617d49ddcdc6ae497259756483016
Lucio-Dev <luciogabrieldeazevedoo@gmail.com>
2025-09-04 20:06:02 -0300

    feat(roles): create table of balances and model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,10 +14,21 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        
+        User::factory()->create([
+            'name'      => 'Usuario padrão',
+            'email'     => 'user@user.com',
+            'password'  => 'user1234',
+            'user_type' => 'user',
+            'cpf'       => '000.000.000-00',
+        ]);
 
         User::factory()->create([
-            'name'  => 'Test User',
-            'email' => 'test@example.com',
+            'name'      => 'Usuario lojista',
+            'email'     => 'shopkeeper@shopkeeper.com',
+            'password'  => 'user1234',
+            'user_type' => 'shopkeeper',
+            'cpf'       => '000.000.000-01',
         ]);
     }
 }

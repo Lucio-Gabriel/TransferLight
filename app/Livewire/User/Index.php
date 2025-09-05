e6fc420e91a54a67ab301389f8c9758db118aa67
Lucio-Dev <luciogabrieldeazevedoo@gmail.com>
2025-09-05 14:00:52 -0300

    feat(roles): create ui of balances

--- app/Livewire/User/Index.php
+++ app/Livewire/User/Index.php
@@ -2,11 +2,26 @@
 
 namespace App\Livewire\User;
 
+use Illuminate\Support\Collection;
+use Illuminate\View\View;
+use Livewire\Attributes\Computed;
 use Livewire\Component;
 
 class Index extends Component
 {
-    public function render()
+    #[Computed]
+    public function balances(): Collection
+    {
+        return auth()->user()->balances()->get();
+    }
+
+    #[Computed]
+    public function hasBalance(): bool
+    {
+        return auth()->user()->balances()->exists();
+    }
+
+    public function render(): View
     {
         return view('livewire.user.index');
     }
--- app/Models/Balance.php
+++ app/Models/Balance.php
@@ -4,12 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Balance extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-        'money',
+        'value',
+        'user_id',
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }

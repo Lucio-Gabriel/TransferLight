c3c5c037747b1164a73cca0d7b59b4cdc7b8658d
Lucio-Dev <luciogabrieldeazevedoo@gmail.com>
2025-09-06 22:42:33 -0300

    feat(transfer): implement shopkeeper balance display and enhance transfers received section

--- resources/views/livewire/shopkeeper/index.blade.php
+++ resources/views/livewire/shopkeeper/index.blade.php
@@ -13,7 +13,9 @@
                     <div class="mr-10">
                         <h3 class="text-xl text-center font-semibold mb-1">Total Recebido</h3>
 
-                        <p class="text-center font-semibold text-green-500">R$ 1,234.56</p>
+                        <p class="text-center font-semibold text-green-500">
+                            R$ {{ number_format($this->shopkeeperBalance->balance, 2, ',', '.') }}
+                        </p>
                     </div>
                 </div>
             </div>
@@ -21,7 +23,7 @@
 
         <div class="max-w-5xl mx-auto sm:px-6 lg:px-8 mt-10">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg ">
-               <div class="flex justify-between items-center p-6 text-gray-900">
+                <div class="flex justify-between items-center p-6 text-gray-900">
                     <div>
                         <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
                         <div class="relative">
@@ -33,103 +35,43 @@
                             <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-300 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Nome..." required />
                         </div>
                     </div>
-               </div>
+                </div>
 
                 <h3 class="text-xl font-semibold mb-1 p-6 text-gray-900">Últimas Transações Recebidas</h3>
 
-                <div class="mb-1 p-6 space-y-4 overflow-auto max-h-96">
-                    <ul class="w-full divide-y divide-gray-200 dark:divide-gray-700">
-                        <li class="pb-3 sm:pb-4">
-                            <div class="flex items-center space-x-4 rtl:space-x-reverse">
-                                <div class="shrink-0">
-                                    <img class="w-8 h-8 rounded-full" src="{{ asset('images/user.png') }}" alt="Neil image">
-                                </div>
-                                <div class="flex-1 min-w-0">
-                                    <p class="text-sm font-medium text-gray-900 truncate">
-                                    Neil Sims
-                                    </p>
-                                    <p class="text-sm text-gray-500 truncate dark:text-gray-400">
-                                    email@flowbite.com
-                                    </p>
-                                </div>
-                                <div class="inline-flex items-center text-base font-semibold text-gray-900">
-                                    $320
-                                </div>
-                            </div>
-                        </li>
-                        <li class="py-3 sm:py-4">
-                            <div class="flex items-center space-x-4 rtl:space-x-reverse">
-                                <div class="shrink-0">
-                                    <img class="w-8 h-8 rounded-full" src="{{ asset('images/user.png') }}" alt="Neil image">
-                                </div>
-                                <div class="flex-1 min-w-0">
-                                    <p class="text-sm font-medium text-gray-900 truncate">
-                                    Neil Sims
-                                    </p>
-                                    <p class="text-sm text-gray-500 truncate dark:text-gray-400">
-                                    email@flowbite.com
-                                    </p>
-                                </div>
-                                <div class="inline-flex items-center text-base font-semibold text-gray-900">
-                                    $320
-                                </div>
-                            </div>
-                        </li>
-                        <li class="py-3 sm:py-4">
-                        <div class="flex items-center space-x-4 rtl:space-x-reverse">
-                            <div class="shrink-0">
-                                <img class="w-8 h-8 rounded-full" src="{{ asset('images/user.png') }}" alt="Neil image">
-                            </div>
-                            <div class="flex-1 min-w-0">
-                                <p class="text-sm font-medium text-gray-900 truncate">
-                                Neil Sims
-                                </p>
-                                <p class="text-sm text-gray-500 truncate dark:text-gray-400">
-                                email@flowbite.com
-                                </p>
-                            </div>
-                            <div class="inline-flex items-center text-base font-semibold text-gray-900">
-                                $320
-                            </div>
+                <div class="overflow-auto max-h-96">
+                    @foreach ($this->transfersReceived as $transfer)
+                        <div class="mb-1 p-6 space-y-4 overflow-auto max-h-96">
+                            <ul class="w-full divide-y divide-gray-200 dark:divide-gray-700">
+                                <li class="pb-3 sm:pb-4">
+                                    <div class="flex items-center space-x-4 rtl:space-x-reverse">
+                                        <div class="shrink-0">
+                                            <img class="w-8 h-8 rounded-full" src="{{ asset('images/user.png') }}" alt="Neil image">
+                                        </div>
+                                        <div class="flex-1 min-w-0">
+                                            <p class="text-sm font-medium text-gray-900 truncate">
+                                                {{ $transfer->user->name }}
+                                            </p>
+                                            <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+                                                {{ $transfer->user->email }}
+                                            </p>
+                                        </div>
+                                        <div class="flex-1 min-w-0">
+                                            <p class="text-sm font-medium text-gray-900 truncate">
+                                                Recebido em:
+                                            </p>
+                                            <p class="text-sm text-gray-500 truncate dark:text-gray-400 ml-1">
+                                                {{ $transfer->created_at->format('d/m/Y') }}
+                                            </p>
+                                        </div>
+                                        <div class="inline-flex items-center text-base font-semibold text-gray-900">
+                                            $ {{ number_format($transfer->value, 2, ',', '.') }}
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
                         </div>
-                    </li>
-                    <li class="py-3 sm:py-4">
-                        <div class="flex items-center space-x-4 rtl:space-x-reverse">
-                            <div class="shrink-0">
-                                <img class="w-8 h-8 rounded-full" src="{{ asset('images/user.png') }}" alt="Neil image">
-                            </div>
-                            <div class="flex-1 min-w-0">
-                                <p class="text-sm font-medium text-gray-900 truncate">
-                                Neil Sims
-                                </p>
-                                <p class="text-sm text-gray-500 truncate dark:text-gray-400">
-                                email@flowbite.com
-                                </p>
-                            </div>
-                            <div class="inline-flex items-center text-base font-semibold text-gray-900">
-                                $320
-                            </div>
-                        </div>
-                    </li>
-                    <li class="py-3 sm:py-4">
-                        <div class="flex items-center space-x-4 rtl:space-x-reverse">
-                            <div class="shrink-0">
-                                <img class="w-8 h-8 rounded-full" src="{{ asset('images/user.png') }}" alt="Neil image">
-                            </div>
-                            <div class="flex-1 min-w-0">
-                                <p class="text-sm font-medium text-gray-900 truncate">
-                                Neil Sims
-                                </p>
-                                <p class="text-sm text-gray-500 truncate dark:text-gray-400">
-                                email@flowbite.com
-                                </p>
-                            </div>
-                            <div class="inline-flex items-center text-base font-semibold text-gray-900">
-                                $320
-                            </div>
-                        </div>
-                    </li>
-                    </ul>
+                    @endforeach
                 </div>
             </div>
         </div>

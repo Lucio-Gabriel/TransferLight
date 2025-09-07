a787130dbeb0b0771c81313f7e740e29ccd01a6c
Lucio-Dev <luciogabrieldeazevedoo@gmail.com>
2025-09-06 21:55:36 -0300

    feat(trasnfer): update and create method transfer and balance

--- resources/views/livewire/transfer/create.blade.php
+++ resources/views/livewire/transfer/create.blade.php
@@ -34,7 +34,7 @@
                         </div>
                         <div>
                             <label for="value" class="block text-sm font-medium text-gray-700 dark:text-gray-300">Valor</label>
-                            <input wire:model="value" type="number" id="value" step="0.01" placeholder="R$ 0,00"
+                            <input wire:model="value" type="number" id="value" step="0.01" placeholder="R$ 0,00"  x-data x-mask:dynamic="$money($input, ',', '.')"
                                 class="mt-1 w-full rounded-lg border border-gray-300 bg-white px-3 py-2 text-sm shadow-sm focus:border-primary focus:ring focus:ring-primary/30 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-100">
 
                             <div class="mt-2 text-red-500 text-sm">
